83c42fcfa4003047755a680ca3e54de2f838f5f0
Ramona Maltan <ramonamaltan@gmail.com>
2021-04-03 11:24:26 +0200

    base64 encoding and decoding

---
+++ 11-base64/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"log"
+)
+
+func main() {
+	s := "Love is but a song to sing Fear's the way we die You can make the mountains ring Or make the angels cry Though the bird is on the wing And you may not know why Come on people now Smile on your brother Everybody get together Try to love one another Right now"
+
+	// set own encoding standard
+	// encodeStd := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+	// s64 := base64.NewEncoding(encodeStd).EncodeToString([]byte(s))
+
+	s64 := base64.StdEncoding.EncodeToString([]byte(s))
+
+	fmt.Println(len(s))
+	fmt.Println(len(s64))
+	fmt.Println(s)
+	fmt.Println(s64)
+
+	bs, err := base64.StdEncoding.DecodeString(s64)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(string(bs))
+}
